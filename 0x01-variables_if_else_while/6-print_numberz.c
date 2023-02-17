25a4797ae09be03c355dda4cfdf72c271f045e71
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-02-17 13:04:47 -0800

    all

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry poinit
  *
  * variable : n as int
  * Description: prints 0 t 9
- * Return : always 0
+ * Return: always 0
  */
 
 int main(void)
@@ -15,6 +15,5 @@ int main(void)
 	for (n = 48; n < 58; n++)
 		putchar(n);
 	putchar('\n');
-	
 	return (0);
 }
