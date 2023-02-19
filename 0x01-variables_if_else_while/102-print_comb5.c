876f060e0eeee32056d01841b72642b9c832aab7
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-02-19 08:09:54 -0800

    task 12 tuff

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,31 +3,37 @@
 /**
  * main - Entry point
  *
-i* Return: prints doub;e digit from 00 - 99
+ * Resturn: prints number from 00 to 99
  */
 
 int main(void)
 {
+	int n, z, y, p;
 
-        int n, z;
+	for (n = 48; n <= 57; n++)
+	{
+		for (z = 48; z <= 56; z++)
+		{
+			for (y = 48; y <= 57; y++)
+			{
+				for (p = 48; p <= 57; p++)
+				{
+					putchar(n);
+					putchar(z);
+					putchar(' ');
+					putchar(y);
+					putchar(p);
 
-        for (n = 48; n <= 57 ; n++)
-        {
-                for (z = n ; z <= 57; z++)
-                {
-                      
-                            putchar(n);
-                            putchar(z);
-                        
-                      
-                      
+					if (n != 57 || z != 56 || y != 57 || p != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
 
-                        if (n != 57 || z != 57 )
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                }
-        }
-        return (0);
+	return (0);
 }
