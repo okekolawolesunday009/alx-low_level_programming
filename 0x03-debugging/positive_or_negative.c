2f55f101652ffd81a6f667a9b5c71ca2b5cca99e
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-02-21 04:15:52 -0800

    ago

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all form of n
+ * Return: Always 0 (suceess)
+ */
+
+void positive_or_negative(int i)
+{
+	if(i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+}
