22f9c1acfa655835cc066d113b19326f8a4a3a02
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-02-21 13:13:57 -0800

    push

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Entry point
+ *
+ * Return: always 0
+ */
+
+int _abs(int n)
+{
+/**int n;**/
+	
+	n = abs(n);
+
+	return (n);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit- Entry point
+ *
+ * Return: suceeess
+ */
+
+int print_last_digit(int l){
+	int n;
+
+	n = l % 10;
+	_putchar(n);
+}
