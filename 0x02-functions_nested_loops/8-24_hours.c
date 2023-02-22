08e06be9d250f5facbd4d70170b5b03021d7254b
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-02-21 22:29:09 -0800

    alx de ment

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,31 +1,30 @@
 #include "main.h"
 
-/** 
- * jack_bauer(void) - Entry
+/**
+ * jack_bauer - Entry point
  *
- * Return: 00 - 23: 59
+ * Return: 00:00  - 23: 59
  */
 
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int a;
+	int b;
+	int min_rem;
+	int hours_rem;
 
-	for (a = 0; a <= 2; a++)
+	for (a = 0; a <= 23; a++)
 	{
-		for (b = 0; b <= 3; b++)
+		for (b = 0; b <= 59; b++)
 		{
-			for (c = 0; c <= 5; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(':');
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
-				}
-			}
+			min_rem = b % 10;
+			hours_rem = a % 10;
+			_putchar(a / 10 + '0');
+			_putchar(hours_rem + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(min_rem + '0');
+			_putchar('\n');
 		}
 	}
 }
