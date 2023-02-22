89416df6d0f1a757ab2543d2d20eef9b7ff03e06
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-02-22 10:57:55 -0800

    code wars

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * void print_to_98 - function prints n to 98
- *
- * Return: Always suuccess
+ * print_to_98 - function prints from n to 98
+ * @n: parameter used
+ * Return: Always 0
  */
 
 void print_to_98(int n)
 {
 	int z, i;
+
 	n = n;
 	z = 98;
 
-	if (n >= 98)
+	if (n < 98)
 	{
 		for (i = n; i <= z; i++)
 		{
@@ -23,19 +24,19 @@ void print_to_98(int n)
 				printf(" ");
 			}
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 	else
 	{
-		for (i = n; i >= z; i--)
+		for (i = n; i >= z; --i)
 		{
 			printf("%d", i);
 			if (i != 98)
 			{
-				printf(".");
+				printf(",");
 				printf(" ");
 			}
 		}
-		printf("\n");
+		putchar('\n');
 	}
 }
