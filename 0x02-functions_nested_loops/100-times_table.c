9069d41c41ffdc6abc0dcd36518c93ca7c9847fa
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-02-23 02:38:32 -0800

    all

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,8 @@
 void print_times_table(int n)
 {
 	int i, j, p;
-	if (n < 15)
+
+	if (n >= 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -23,27 +24,27 @@ void print_times_table(int n)
 				if (p <= 9)
 				{
 					_putchar(' ');
+					_putchar(' ');
+					_putchar((p % 10) + '0');
 				}
-				if (p <= 99)
+				else if (p >= 10 && p < 100)
 				{
 					_putchar(' ');
+					_putchar((p / 10) % 10 + '0');
+					_putchar(p % 10 + '0');
 				}
-				if ( p <= 100)
+				else if (p > 99 && p < 1000)
 				{
 					_putchar((p / 100) + '0');
+					_putchar((p / 10) % 10 + '0');
 					_putchar((p  % 10) + '0');
 				}
-				else if (p <= 99 && p <= 100)
+				else
 				{
 					_putchar((p / 10) + '0');
 				}
-				_putchar((p % 10) + '0');
 			}
 			_putchar('\n');
 		}
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
