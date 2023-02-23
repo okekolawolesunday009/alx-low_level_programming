58fffb6dda4d7e6c9072467cf679546aae91b9a4
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-02-23 10:05:44 -0800

    mul, num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 - 9
+ *
+ * Return: Always success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
