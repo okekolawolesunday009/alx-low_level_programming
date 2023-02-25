bc3affb60644c89c499144bfb134c439265fac2d
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-02-24 18:42:42 -0800

    all

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,17 +7,17 @@
 
 int main(void)
 {
-/**	int i;**/
-	
+       /**int i;**/
+	/****/
 	printf("Infinite loop incoming :(\n");
-
-/**	i = 0;**/
-	 /**
+	/**
+	 * i = 0;
+	 *
 	 * while (i < 0)
 	 * {
-	 * 	putchar(i);
-	 * } **/
-
+	 * putchar(i);
+	 * }
+	 */
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
