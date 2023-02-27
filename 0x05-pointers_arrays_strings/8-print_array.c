2791f3c03bce9cc67ea4fb1454614b04496d0177
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-02-27 11:56:56 -0800

    all

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - print the n of array with a comma
+ * @n: int a
+ * @n: int n
+ *
+ * Return: Always 0
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			_putchar(',');
+		}
+	}
+	_putchar('\n');
+}
