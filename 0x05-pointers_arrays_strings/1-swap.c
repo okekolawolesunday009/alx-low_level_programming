b2e5ae8a73795a3dce1e3245f97aa29db0b63d59
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-02-27 06:08:03 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - function swaps two interger
+ * @a: int a
+ * @b: int b
+ *
+ * Return: Always success
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
