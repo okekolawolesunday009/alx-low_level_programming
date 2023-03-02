efd25eb03b664285375774dea364e9a73ee2fa7c
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-03-02 04:37:19 -0800

    push

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_array - prints rev array
+ * @n: int n
+ * @a: arrays of int
+ * Return: reverse array
+ */
+
+void reverse_array(int *a, int n)
+{
+    int i;
+
+    i = n - 1;
+    while (i >= 0)
+    {
+        if (i != n-1)
+        {
+            printf(", ");
+        }
+        printf("%d", a[i]);
+        i--;
+    }
+    printf("\n");
+}
