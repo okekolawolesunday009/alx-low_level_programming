11911f4d00893764be984f04629173982db0313c
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-03-07 11:19:08 -0800

    @all@

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
 #endif
