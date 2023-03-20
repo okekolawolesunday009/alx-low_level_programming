af1ef78a4ad667581a142ef97a8c7a08253c1957
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-03-20 08:51:30 -0700

    dog 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * File: dog.h
+ * Desc: eader for struct dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
