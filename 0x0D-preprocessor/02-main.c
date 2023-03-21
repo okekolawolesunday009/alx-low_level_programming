c914fe10507e4d4c763a02c9f0a594c5185d540e
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-03-21 00:19:19 -0700

    all

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of file
+ * Desc: name of file
+ * Return: Always success
+ */
+
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of file
+ * Desc: name of file
+ * Return: Always success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
