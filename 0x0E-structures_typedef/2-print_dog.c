cb7a0200694d09145f9331bae78727fd6f5fb6d4
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-03-21 02:02:26 -0700

    all

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,32 +11,33 @@
 
 void print_dog(struct dog *d)
 {
-
-    if (d == NULL){
-      return;
-    }
-    if (d->name == NULL){
-        printf("Name: %s","nill\n");
-    }
-    else{
-        printf("Name: %s\n", d->name);
-
-    }
-    if (d->age < 0){
-        printf("Age: %s","nill\n");
-    }
-    else{
-        printf("Age: %f\n", d->age);
-
-    }
-    if (d->owner == NULL){
-        printf("Name: %s","nill\n");
-    }
-    else{
-        printf("Name: %s\n", d->owner);
-
-    }
-
-
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		printf("Name: ,(nill)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	if (d->age < 0)
+	{
+		printf("Age: (nill)\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+	if (d->owner == NULL)
+	{
+		printf("Name: (nill)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->owner);
+	}
 }
 
