1e21725ce5f84e6c08e65a883e27db13570562ac
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-03-21 01:44:34 -0700

    al

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
+/*
  * File: dog.h
- * Desc: eader for struct dog
+ * Desc: Header file that defines a new type struct dog.
+ */
+
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
 
 struct dog
@@ -13,5 +20,9 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
 #endif
