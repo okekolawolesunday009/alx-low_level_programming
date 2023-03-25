59a023adedc62629cc195f876ad8ded2a7e52084
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-03-24 21:06:31 -0700

    my first function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * print_name - prints the name (function pointer
+ * @name: parameter used
+ * @f: parameter
+ *
+ * Return: returns name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return;
+	f(name);
+}
