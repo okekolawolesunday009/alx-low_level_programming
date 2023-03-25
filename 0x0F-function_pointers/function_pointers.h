5fac5aa111ecb041286d941454f2fd95fe9db0e2
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-03-25 02:10:55 -0700

    all

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -9,4 +9,5 @@ void print_elem_hex(int elem);
 void print_elem(int elem);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
 #endif
