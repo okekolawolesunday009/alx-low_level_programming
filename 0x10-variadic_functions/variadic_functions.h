3984787ded813c9d40451a7db3469e0c97aacfaa
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-03-25 11:17:53 -0700

    good new

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,10 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg)
 
 #endif
 
