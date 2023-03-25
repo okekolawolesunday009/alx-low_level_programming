e61dfc0fe30ea3dfc1d7c44694399bec165ce7e5
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-03-24 21:53:51 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,7 +21,6 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-void free_dog(dog_t *d);
 /**
  * dog_t - Typedef for struct dog
  */
