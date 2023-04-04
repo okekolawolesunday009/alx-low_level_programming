00738f9c16ebf14cd7f740a858779a13c699dce0
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-04-03 17:43:09 -0700

    i feel i can fly

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,29 +1,26 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-
-
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- *
+ * add_nodeint - adds a new node at the begining
+ * @head: type of listint_t
+ * @n: value or data
+ * Return: returns a new node
  */
-typedef struct listint_s
+
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
+	listint_t *new;
 
-size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(listint_t *head);
+	new = malloc(sizeof(listint_t));
 
-
-
-#endif // LISTS_H
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
