58d4e65ddcadaf009413876dd11b64cc1ff3e6d6
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-04-03 17:30:46 -0700

    make me happy Alx

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,24 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * listint_len - prints the length of node
+ * @h: parameter passed
+ * Return: always node
+ */
+
 size_t listint_len(const listint_t *h)
 {
-    size_t node = 0;
-    while (h)
-    {
-        if (h->n == NULL)
-        {
-            return (NULL);
-        }
-       node++;
-       h = h->next;
+	size_t node = 0;
 
-    }
-    return (node);
+	while (h)
+	{
+		if (h == NULL)
+		{
+			return (NULL);
+		}
+		node++;
+		h = h->next;
+	}
+	return (node);
 }
