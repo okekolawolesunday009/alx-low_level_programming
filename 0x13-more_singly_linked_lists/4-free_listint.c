f64e4045014b4c05f95942c6ea147aad6fa0ff61
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-04-03 17:51:30 -0700

    free head

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,9 +2,15 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * free_list - frees the memory
+ * @head: parmeter
+ */
+
 void free_list(listint_t *head)
 {
 	listint_t *temp;
+
 	while (head)
 	{
 		temp = temp->next;
