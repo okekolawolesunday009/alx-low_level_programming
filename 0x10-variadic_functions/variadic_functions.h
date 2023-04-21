b8767ae16d5a508d387edfd7cf5ab0b898d31d6f
OkeKolawoleSunday009 <Okekolawolsunday@gmail.com>
2023-04-21 06:17:12 -0700

    apply

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,7 +7,7 @@ typedef struct operations
 {
 	char *identifier;
 	void (*print)(va_list lists);
-}print_t;
+} print_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
