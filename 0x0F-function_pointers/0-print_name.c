376d1c41d72923240edd880f5572d7b8c9fe6bf7
OkeKolawoleSunday009 <Okekolawolsunday@gmail.com>
2023-04-20 17:44:57 -0700

    feels good to be back

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name
+ * @name: parameter involved
+ * @f: f is a function call back
+ *
+ * Return: returns the value of name
+ */
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return;
+	f(name);
+}
