ca4b5710e6160b4cbaa47d09f00dd75accddd881
OkeKolawoleSunday009 <Okekolawolsunday@gmail.com>
2023-04-20 18:00:53 -0700

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - loops through array to print
+ * @array: datas to print
+ * @size: size of arry
+ * @action: call back function
+ *
+ * Return: returns the values in the array
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		(*action)(array[i]);
+
+}
