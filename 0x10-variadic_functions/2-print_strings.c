79ac160c148e302447cf9bf35e6f40f77d2b2f4d
OkeKolawoleSunday009 <Okekolawolsunday@gmail.com>
2023-04-21 17:58:49 -0700

    nil

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		string = va_arg(list, char *);
 		if (string == NULL)
-			printf("(nill)");
+			printf("(nil)");
 		else
 			printf("%s", string);
 		if (index != (n - 1) && separator != NULL)
