6cc0801abd526f3d6f3f3e7bba67601a660ba095
OkeKolawoleSunday009 <Okekolawolsunday@gmail.com>
2023-04-28 05:58:26 -0700

    holberton

---
+++ 0x12-singly_linked_lists/holberton.asm
@@ -0,0 +1,20 @@
+section .data
+	hello: db 'Hello, Holberton'
+oAh, 00
+
+section .text
+	global main
+	extern printf
+
+main: 
+	push rbp
+	mov rbp, rsp
+
+	mov rdi, hello
+	xor rax, rax
+	call printf
+
+	mov resp, rdp
+	pop rdp
+	xor eax, eax
+	ret
