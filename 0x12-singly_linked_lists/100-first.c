c970e15756cedd2f57c78295529b92c922e84474
OkeKolawoleSunday009 <Okekolawolsunday@gmail.com>
2023-04-28 05:41:02 -0700

    push

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * @void: void
  */
 
-void printFirst(void)__attribute__ ((constructor))
+void printFirst(void)__attribute__ ((constructor));
 void printFirst(void)
 {
 	printf("You're beat! and yet, you must allow,\n
