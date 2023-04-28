3882f981ecc33b5953d93c3806667d5f15a9b5f8
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-04-28 01:41:01 +0100

    list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef LIST_H
-#define LIST_H
-
+#define LIST_H`
 #include <stdlib.h>
 #include <stdio.h>
 /**
@@ -13,11 +12,10 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
-
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
