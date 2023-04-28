975ca987ba1481b1c6649bb5169030176f75748c
OkeKolawoleSunday009 <Okekolawolsunday@gmail.com>
2023-04-27 22:06:20 -0700

    betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,8 +5,9 @@
  * @void: void
  */
 
-void printFirst(void)__attribute__ ((constructor));
+void printFirst(void)__attribute__ ((constructor))
 void printFirst(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
