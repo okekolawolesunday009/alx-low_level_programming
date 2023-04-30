75675a7efa8c9c632f7c565b1cb02062821acdd7
OkeKolawoleSunday009 <Okekolawolsunday@gmail.com>
2023-04-30 09:51:25 -0700

    kola

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-        size_t n = 0;
-        while (h)
-        {
-                if (h == NULL)
-                        return (1);
-                else
-                        printf("%d\n",h->n);
-                n++;
-                h = h->next;
-        }
-        return (n);
+	size_t n = 0;
+
+	while (h)
+	{
+		if (h == NULL)
+			return (1);
+		printf("%d\n", h->n);
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
