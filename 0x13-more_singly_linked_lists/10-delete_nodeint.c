499516918b533a33d75b472af3d535b015da759a
OkeKolawoleSunday009 <Okekolawolsunday@gmail.com>
2023-04-30 17:30:02 -0700

    100

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,24 +13,20 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i;
 	listint_t *prev, *update;
 
-	update = malloc(sizeof(lisint_t));
+	update = malloc(sizeof(listint_t));
 
 	update = *head;
 	i = 0;
-	while (i < index && update != NULL; i++)
+	for (;i < index && update != NULL; i++)
 	{
 		prev = update;
 		update = update->next;
-		i++;
 	}
 	if (update == NULL)
 		return (-1);
 	if (prev == NULL)
-		*head = current->next;
-	else
-	{
-		prev->next = update->next;
-		free(update);
-		return (1);
-	}
+		*head = update->next;
+	prev->next = update->next;
+	free(update);
+	return (1);
 }
