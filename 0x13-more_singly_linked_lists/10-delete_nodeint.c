906c8fafcf2e294e112982fee93650773a6f4bae
OkeKolawoleSunday009 <Okekolawolsunday@gmail.com>
2023-05-01 18:59:01 -0700

    task delete

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,8 +3,7 @@
 /**
  * delete_nodeint_at_index - add new node at specific position
  * @head: parameter
- * @idx: position to the new node
- * @n: data or value to be passed
+ * @index: position to the new node
  * Return: return the new list with the addition
  */
 
@@ -15,15 +14,26 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	update = malloc(sizeof(listint_t));
 
+	prev = NULL;
 	update = *head;
+
+	if (update == NULL || index == 0)
+	{
+		if (update != NULL)
+			*head = (*head)->next;
+		free(update);
+		return (1);
+	}
 	i = 0;
-	for (;i < index && update != NULL; i++)
+	for (; i < index && update != NULL; i++)
 	{
 		prev = update;
 		update = update->next;
 	}
 	if (update == NULL)
+	{
 		return (-1);
+	}
 	if (prev == NULL)
 		*head = update->next;
 	prev->next = update->next;
