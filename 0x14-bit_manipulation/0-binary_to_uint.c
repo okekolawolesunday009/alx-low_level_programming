e6a94c361fb707668a4d950799f973b1c7f7a86e
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-05-07 14:51:58 +0100

    manupulate

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,23 +9,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	size_t l = strlen(b), i = 0;
+	size_t i = 0;
 	unsigned int res = 0;
 
-	for ( i = 0; i < l; i++)
-	{
-		if (b[i] != '0' && b[i] != '1')
-		{
-			return (0);
-		}
-	}
-	for (i = 0; i < l; i++)
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '1')
-		{
-			res += (unsigned int) pow(2, l - i -1);
-		}
-	}
+			res |= (1 << (strlen(b) - 1 - i));
+		else if (b[i] != '0')
+			return (0);
+        }
 	return (res);
 }
-
