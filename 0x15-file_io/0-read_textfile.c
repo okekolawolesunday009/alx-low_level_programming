27415789ef75ff9d2bc285932c938e661f1f34a0
OkeKolawoleSunday009 <Okekolawolsunday@gmail.com>
2023-05-09 20:23:21 -0700

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,26 +1,24 @@
 #include "main.h"
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int w, r, o;
+	ssize_t w, r, o;
 	char *charMemory = malloc(sizeof(char) * letters);
 
-	o = open(filename, 0_RDONLY);
+	o = open(filename, O_RDONLY);
 	r = read(o, charMemory, letters);
-	w = write(1, charMemory, letters);
+	w = write(STDOUT_FILENO, charMemory, r);
 
 	if (filename == NULL)
 		return (0);
+	if (o == -1)
+		return (0);
 	if (charMemory == NULL)
 		return (0);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(charMemory);
-		return (0);
-	}
 	free(charMemory);
 	close(o);
-
 	return (w);
 }
