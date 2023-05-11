65d9dd2b52cd800195b9f2a3ecdb74b53bb87086
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-05-11 17:23:03 +0100

    shelll

--- shell/acAV.c
+++ shell/acAV.c
@@ -2,11 +2,26 @@
 
 int main(int ac, char **av)
 {
-       	int i;
-    printf("Number of arguments: %d\n", ac);
-    for (i = 0; i < ac; i++)
-    {
-        printf("Argument %d: %s\n", i, av[i]);
-    }
-    return (0);
+	int i;
+	pid_t pid = fork();
+	
+	if (pid == 0)
+	{
+		printf("Child PID is %d\n", getpid());
+	}
+	else if (pid > 0) {
+		printf("Parent PID is %d, child PID is %d\n", getpid(), pid);
+	}
+	else
+	{
+		perror("fork");
+		return 1;
+	}
+
+	printf("Number of arguments: %d\n", ac);
+	for (i = 0; i < ac; i++)
+	{
+		printf("Argument %d: %s\n", i, av[i]);
+	}
+	return (0);
 }
