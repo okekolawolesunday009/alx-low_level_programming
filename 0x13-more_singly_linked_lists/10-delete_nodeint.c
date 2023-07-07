4b60807f489aa768eab8d9683ba0f07a02ecb716
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-07-07 09:07:12 +0100

    10-deleteedit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
         *head = (*head)->next;
 		free(update);
 		update = NULL;
-		return (1);
+		return (-1);
 	}
 	else
     {
