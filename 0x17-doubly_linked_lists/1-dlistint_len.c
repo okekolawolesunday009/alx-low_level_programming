7374a786392f856572b174ea344f001940dc7a03
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-17 03:12:14 +0100

    hold

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+
+size_t dlistint_len(const dlistint_t *h)
+{
+
+    size_t n = 0;dd
+
+	while (h)
+	{
+		if (h == NULL)
+			return (1);
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
