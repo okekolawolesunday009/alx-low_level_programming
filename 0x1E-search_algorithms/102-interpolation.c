6dba3dbc388ea34edae8c34116c3cf75712007eb
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-12-15 05:35:11 +0000

    updat 100-O

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,11 @@
+#include "search_algos.h"
+/**
+ *  interpolation_search - searches in a sorted array
+ *  @array: set of array
+ *  @size: the size of the array
+ *  @value: value to be search
+ *  Return: result or none
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+}
